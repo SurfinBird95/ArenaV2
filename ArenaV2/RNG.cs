3dd12faa96d6ea90d1f3c618b57868a5259834be
Tomáš Straka <tomasstrakabusiness@gmail.com>
2025-02-17 14:34:29 +0100

    Created RNG class Before rework of RNG in game

---
+++ ArenaV2/RNG.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaV2
+{
+    public class RNG
+    {
+        private int lowerRangeInt {get;set;}
+
+        private int upperRangeInt {get;set;}
+
+        private Random random;
+
+        public int createRandomIntNumber(int lowerRangeInt, int upperRangeInt)
+        {
+            return random.Next(lowerRangeInt, upperRangeInt);
+        }
+        
+    }
+}
